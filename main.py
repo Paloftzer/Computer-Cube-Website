4deb6b57d49b020df4a643e4aad4962099943276
Marwin Scherrer <Marwin@scherrer.se>
2021-10-19 10:38:08 +0200

    Added comment examples

--- main.py
+++ main.py
@@ -24,3 +24,10 @@ def login_page():
 
 if __name__ == "__main__":
     app.run(debug = True)
+    
+#! This is an alert
+#? This is a query
+#* Highlighting for important information
+#TODO: Learn what TODO means
+## Removed comment
+# Regular comment
